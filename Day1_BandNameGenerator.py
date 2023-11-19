f50425432201e82113da89c4df8282b661030edc
Vineet Joshi <110726731+vjoshi253@users.noreply.github.com>
2023-11-19 21:55:47 +0530

    Day 1: Band Name Generator

---
+++ Day1_BandNameGenerator.py
@@ -0,0 +1,11 @@
+# Greetings for the users.
+print("Welcome to the Brand Name Generator.")
+
+# Get the name of the city the user is born in.
+city_name = input("What's the name of the city you grew up in?\n")
+
+# Get the user's pet name.
+pet_name = input("What's your pet's name?\n")
+
+# Concatinate the above two information to generate the band name for the user. 
+print("Your band name could be",city_name,pet_name,end = "")
